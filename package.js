22143e5c0bd4eab651c6451f8247542582f4282a
Taras Dolynyuk <trsdln@gmail.com>
2017-03-08 15:44:03 +0200

    Adjust deps

--- package.js
+++ package.js
@@ -1,17 +1,18 @@
 Package.describe({
     summary: "Login service for Azure Active Directory",
-    version: "1.0.0",
-    name: "wiseguyeh:accounts-azure-active-directory",
-    git: "https://github.com/djluck/accounts-azure-active-directory"
+    version: "1.0.1",
+    name: "jss:accounts-azure-active-directory",
+    git: "https://github.com/JSSolutions/accounts-azure-active-directory"
 });
 
 Package.onUse(function(api) {
     api.use(['underscore@1.0.1', 'random@1.0.1']);
     api.use('accounts-base@1.1.3', ['client', 'server']);
+
     // Export Accounts (etc) to packages using this one.
     api.imply('accounts-base', ['client', 'server']);
     api.use('accounts-oauth@1.1.3', ['client', 'server']);
-    api.use('wiseguyeh:azure-active-directory@1.0.0', ['client', 'server']);
+    api.use('jss:azure-active-directory@1.0.1', ['client', 'server']);
 
     api.addFiles('azure_ad_login_buttons.css', 'client');
 
